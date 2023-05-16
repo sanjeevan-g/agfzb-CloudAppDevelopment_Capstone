a64a9e546b8afbb153091f1cab0eb335731b037a
sanjeevan <sanjeevan.g0@gmail.com>
2023-05-16 06:29:16 -0400

    static sites

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,27 +1,70 @@
 from django.db import models
 from django.utils.timezone import now
-
+import datetime
 
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=20, primary_key=True)
+    desc = models.TextField()
+    def __str__(self):
+        return "Name: " + self.name + "," \
+                "Description: " + self.desc
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
-
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealer_id = models.IntegerField(null=False)
+    name = models.CharField(null=False, max_length=20, primary_key=True)
+    SUV = 'suv'
+    SEDAN = 'sedan'
+    WAGON = 'wagon'
+    JEEP = 'jeep'
+    TYPE_CHOICES = [
+        (SUV, 'SUV'),
+        (SEDAN, 'Sedan'),
+        (WAGON, 'Wagon'),
+        (JEEP, 'Jeep')
+    ]
+    model_type = models.CharField(max_length=5, choices=TYPE_CHOICES)
+    year = models.IntegerField(default=datetime.date.today().year) 
+    
+    def __str__(self):
+        return "Make: " + self.make.name + "," \
+                "Name: " + self.name + "," \
+                "Year: " + str(self.year) + "," \
+                "Type: " + self.model_type + "," \
+                "Dealer ID: " + str(self.dealer_id)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
+class CarDealer:
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        self.address = address
+        self.city = city
+        self.full_name = full_name
+        self.id = id
+        self.lat = lat
+        self.long = long
+        self.short_name = short_name
+        self.st = st
+        self.zip = zip
+    
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+    def __init__(self, name, dealership, review, sentiment, purchase, purchase_date=None, car_make=None, car_model=None, car_year=None):
+        self.name = name
+        self.dealership = dealership
+        self.review = review
+        self.sentiment = sentiment
+        self.purchase = purchase
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year =  car_year
+    
+    def __str__(self):
+        return "Review: " + self.review
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,9 +1,10 @@
+from operator import truediv
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarDealer, DealerReview, CarModel, CarMake
+from .restapis import get_dealers_from_cf,get_dealer_reviews_from_cf,post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -13,34 +14,72 @@ import json
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
 # Create your views here.
-
-
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    context = {}
+    return render(request, 'djangoapp/about.html', context)
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    context = {}
+    return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'djangoapp/registration.html', context)
+    else:
+        return render(request, 'djangoapp/registration.html', context)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    elif request.method == 'POST':
+        # Check if user exists
+        username = request.POST['username']
+        password = request.POST['password']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.error("New user")
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            context['message'] = "User already exists."
+            return render(request, 'djangoapp/registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
+        url = "https://27543fa8.us-south.apigw.appdomain.cloud/api/dealership"
+        dealerships = get_dealers_from_cf(url)
+        context = {}
+        context["dealerships"] = dealerships
         return render(request, 'djangoapp/index.html', context)
 
 
@@ -48,7 +87,53 @@ def get_dealerships(request):
 # def get_dealer_details(request, dealer_id):
 # ...
 
+def get_dealer_details(request, dealer_id):
+    if request.method == "GET":
+        url = "https://27543fa8.us-south.apigw.appdomain.cloud/api/get-review"
+        reviews = get_dealer_reviews_from_cf(url, dealer_id)
+        context = {
+            "reviews": reviews,
+            "dealer_id": dealer_id
+        }
+        print(context)
+        return render(request, 'djangoapp/dealer_details.html', context)
+        
+
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
 
+def add_review(request, dealer_id):
+    if request.user.is_authenticated:
+        context = {}
+        dealer_url = "https://27543fa8.us-south.apigw.appdomain.cloud/api/dealership"
+        dealer = get_dealer_by_id_from_cf(dealer_url, dealer_id)
+        context["dealer"] = dealer
+        if request.method == "GET":
+            cars = CarModel.objects.all()
+            context["cars"] = cars
+            print(context)
+            return render(request, 'djangoapp/add_review.html', context)
+        
+        if request.method == "POST":
+            review = {}
+            review["name"] = request.user.first_name + " " + request.user.last_name
+            form = request.POST
+            review["dealership"] = dealer_id
+            review["review"] = form["content"]
+            if(form.get("purchasecheck") == "on"):
+                review["purchase"] = True
+            else:
+                review["purchase"] = False
+            if(review["purchase"]):
+                review["purchase_date"] = datetime.strptime(form.get("purchasedate"), "%m/%d/%Y").isoformat()
+                car = CarModel.objects.get(pk=form["car"])
+                review["car_make"] = car.make.name
+                review["car_model"] = car.name
+                review["car_year"] = car.year
+            post_url = "https://27543fa8.us-south.apigw.appdomain.cloud/api/post-review"
+            json_payload = { "review": review }
+            post_request(post_url, json_payload, dealer_id=dealer_id)
+            return redirect("djangoapp:dealer_details", dealer_id=dealer_id)
+    else:
+        return redirect("/djangoapp/login")
